a2e7a2735f1d4868a4184742b431968862945f0a
opdshe <opdshe@naver.com>
2021-01-12 23:22:08 +0900

    add: static 파일 업로드

--- src/main/java/com/fakebook/dongheon/Application.java
+++ src/main/java/com/fakebook/dongheon/Application.java
@@ -1,13 +1,13 @@
-package com.fakebook.dongheon.fakebook;
+package com.fakebook.dongheon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakebookApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FakebookApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
